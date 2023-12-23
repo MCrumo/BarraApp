434358e892fcde30e5ee41527c018bc7dfb9439b
MCrumo <miquel1rr@gmail.com>
2023-12-23 17:06:30 +0100

    Deleted scp

--- scp-docker.sh
+++
@@ -1,3 +0,0 @@
-
-scp /home/miquel/barraApp/cerbot-init.sh /home/miquel/barraApp/docker-compose.yml /home/miquel/barraApp/docker-compose-up.sh /home/miquel/barraApp/docker-compose-clean.sh root@95.217.131.111:/root/barraApp
-scp -r /home/miquel/barraApp/deploy root@95.217.131.111:/root/barraApp
